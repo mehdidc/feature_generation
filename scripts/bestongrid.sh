36168e2e3fc0845f17e64a6195e3cff689f01d62
mehdi cherti <mehdi@cherti.name>
2016-07-28 13:04:31 +0200

    update

--- scripts/bestongrid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-shuf() { perl -MList::Util=shuffle -e 'print shuffle(<>);' "$@"; }
-i=0
-rm *.png
-for v  in *; do i=$((i+1));cp "$v""/0.png" "$i"".png"; done
-#montage -geometry 1:1 $(ls *.png|shuf|head -n 36) out.png
-montage -geometry 1:1 *.png out.png
