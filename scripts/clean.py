584ee85981a29c685bae596fb1acef3f67e2fc4b
mehdi cherti <mehdi@cherti.name>
2016-09-01 20:07:09 +0200

    update

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,30 +1,29 @@
 
 if __name__ == '__main__':
-	from lightjob.cli import load_db
-	from lightjob.db import SUCCESS
-	import shutil
-	import os
-	db = load_db()
-	def rm(path):
-		if not os.path.exists(path):
-			return
-		print('Removing {}'.format(path))
-		if os.path.isdir(path):
-			shutil.rmtree(path)
-		else:
-			os.remove(path)
+    from lightjob.cli import load_db
+    from lightjob.db import SUCCESS
+    import shutil
+    import os
+    db = load_db()
+    def rm(path):
+        if not os.path.exists(path):
+            return
+        print('Removing {}'.format(path))
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
 
-	for j in db.jobs_with(state=SUCCESS, type="generation"):
-		j = dict(j)
-		s = j['content']['model_summary']
- 		ref_job = db.get_job_by_summary(s)
-		model_details = ref_job['content']
- 		# training job
-		folder = 'jobs/results/{}'.format(ref_job['summary'])
-		#rm(os.path.join(folder, 'model.pkl'))
-		rm(os.path.join(folder, 'features'))
-		rm(os.path.join(folder, 'recons'))
-		rm(os.path.join(folder, 'out'))
-		# generation job of the training job
-		folder = 'jobs/results/{}'.format(j['summary'])
-		rm(os.path.join(folder, 'iterations'))
+    for j in db.jobs_with(state=SUCCESS, type="generation"):
+        j = dict(j)
+        # generation job of the training job
+        folder = 'jobs/results/{}'.format(j['summary'])
+        rm(os.path.join(folder, 'iterations'))
+
+    for j in db.jobs_with(state=SUCCESS, type="training"):
+        j = dict(j)
+        # training job
+        folder = 'jobs/results/{}'.format(j['summary'])
+        rm(os.path.join(folder, 'features'))
+        rm(os.path.join(folder, 'recons'))
+        rm(os.path.join(folder, 'out'))
