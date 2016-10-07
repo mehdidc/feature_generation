7bb6ae9cd5138cf97b055c5b40d35620f80fcdb3
mehdi cherti <mehdi@cherti.name>
2016-10-07 10:52:57 +0200

    update

--- data_future.py
+++ data_future.py
@@ -1,25 +1,44 @@
 from itertools import imap
 from functools import partial
+import random
 import glob
+
 from skimage.transform import resize
 from skimage.util import pad
- 
+from skimage.io import imread
+
 import numpy as np
 
 import datakit
 from datakit.helpers import dict_apply, minibatch, expand_dict, data_path
 
+dataset_patterns = {
+    'sketchy': 'sketchy/256x256/sketch/tx_000000000000/**/*.png',
+    'flaticon': 'flaticon/**/png/*.png',
+    'lfw': 'lfw/imgaligned/**/*.jpg',
+    'shoes': 'shoes/ut-zap50k-images/Shoes/**/**/*.jpg',
+    'svhn': 'svhn/**/*.png',
+    'chairs': 'chairs/rendered_chairs/**/renders/*.png',
+    'icons': 'icons/img/*.png',
+    'aloi': 'aloi/png4/**/*.png',
+    'kanji': 'kanji/cleanpngsmall/*.png',
+    'iam': 'iam/**/**/*.png',
+}
+
+
 def apply_to(iterator, fn, cols=None):
     iterator = imap(partial(dict_apply, fn=fn, cols=cols), iterator)
     return iterator
 
-def pipeline_crop(iterator, shape, pos='random', mode='constant', rng=np.random, **kw):
-    crop_ = partial(crop, shape=shape, pos=pos, mode=mode, rng=rng)
-    return apply_to(iterator, crop_, cols=['X'])
+def as_iterator_func(fn):
+    def fn_(iterator, *args, **kwargs):
+        return apply_to(iterator, partial(fn, *args, **kwargs), cols=['X'])
+    return fn_
 
 def crop(img, shape=(1, 1), pos='random', mode='constant', rng=np.random):
-    # assumes img is shape (color, h, w)
-    img_c, img_h, img_w = img.shape
+    # assumes img is shape (h, w, color)
+    assert len(img.shape) == 3 and img.shape[2] in (1, 3)
+    img_h, img_w, img_c = img.shape
     h, w = shape
     if pos == 'random':
         y = rng.randint(0, img_h)
@@ -29,12 +48,16 @@ def crop(img, shape=(1, 1), pos='random', mode='constant', rng=np.random):
         x = img_w // 2
     else:
         raise Exception('Unkown mode')
-    out_img = np.empty((img_c, h, w))
-    img_ = np.empty((img_c, img_h + h, img_w + w))
-    for c in range(img.shape[0]):
-        img_[c] = pad(img[c], (h/2, w/2), str(mode))
-    img = img_[:, y:y+h, x:x+w]
+    out_img = np.empty((h, w, img_c))
+    img_ = np.empty((img_h + h, img_w + w, img_c))
+    for c in range(img_c):
+        img_[:, :, c] = pad(img[:, :, c], (h/2, w/2), str(mode))
+    img = img_[y:y+h, x:x+w, :]
     return img
+
+def pipeline_crop(iterator, shape, pos='random', mode='constant', rng=np.random, **kw):
+    crop_ = partial(crop, shape=shape, pos=pos, mode=mode, rng=rng)
+    return apply_to(iterator, crop_, cols=['X'])
 
 def pipeline_order(iterator, order='th'):
     if order == 'th':
@@ -50,7 +73,7 @@ def pipeline_resize(iterator, shape, **kw):
 def pipeline_invert(iterator, **kw):
     return apply_to(iterator, lambda x:1-x, cols=['X'])
 
-def pipeline_normalize(iterator, value=255., **kw):
+def pipeline_divide_by(iterator, value=255., **kw):
     return apply_to(iterator, lambda x:x/float(value), cols=['X'])
 
 def pipeline_limit(iterator, nb=100):
@@ -59,33 +82,47 @@ def pipeline_limit(iterator, nb=100):
         buffer.append(next(iterator))
     return buffer
 
-def imagecollection_load_as_iterator_(pattern=''):
+def pipeline_shuffle(iterator, rng=random):
+    iterator = list(iterator)
+    rng.shuffle(iterator)
+    return iter(iterator)
+
+def pipeline_imagefilelist(iterator, pattern=''):
+    pattern = pattern.format(**dataset_patterns)
     pattern = data_path(pattern)
     filelist = glob.glob(pattern)
-    return datakit.imagecollection.load_as_iterator(filelist)
+    return iter(filelist)
+
+def pipeline_imageread(iterator):
+    return datakit.imagecollection.load_as_iterator(iterator)
+
+def pipeline_normalize_shape(iterator):
+    # if shape = 2, add a new axis at the right
+    # if shape = 3, leave it as it is
+    return apply_to(iterator, lambda x:x[:, :, np.newaxis] if len(x.shape) == 2 else x, cols=['X'])
+
+def pipeline_load_dataset(iterator, name, *args, **kwargs):
+    assert hasattr(datakit, name)
+    module = getattr(datakit, name)
+    return module.as_iterator(*args, **kwargs)
 
 operators = {
+    'dataset': pipeline_load_dataset,
+    'imagefilelist': pipeline_imagefilelist,
+    'imageread': pipeline_imageread,
     'crop': pipeline_crop,
     'resize': pipeline_resize,
     'invert': pipeline_invert,
-    'normalize': pipeline_normalize,
+    'divide_by': pipeline_divide_by,
     'limit': pipeline_limit,
-    'order': pipeline_order
-}
-
-datasets = {
-    'mnist': datakit.mnist.load_as_iterator,
-    'cifar': datakit.cifar.load_as_iterator,
-    'imagecollection': imagecollection_load_as_iterator_
+    'order': pipeline_order,
+    'normalize_shape': pipeline_normalize_shape,
+    'shuffle': pipeline_shuffle
 }
 
 def loader(params):
-    loader_name = params['name']
-    loader_params = params['params']
     pipeline = params['pipeline']
-    assert loader_name in datasets
-    loader = datasets[loader_name]
-    iterator = loader(**loader_params)
+    iterator = None
     for op in pipeline:
         name, params = op['name'], op['params']
         iterator = operators[name](iterator, **params)
@@ -94,26 +131,23 @@ def loader(params):
 if __name__ == '__main__':
     from tools.brushstroke.common import disp_grid
     from skimage.io import imsave
+    pattern = '{chairs}'
     pipeline = [
-            {'name': 'limit', "params": {"nb": 10}},
-            {'name': 'resize', "params": {'shape': (64, 64)}},
-            {'name': 'order', "params": {"order": "th"}},
-            {'name': 'crop', 'params': {'shape': (32, 32), 'pos': 'random', 'mode': 'constant'}},
-            {'name': 'normalize', "params": {"value": 255}},
-            {'name': 'invert', "params": {}},
+        {'name': 'imagefilelist', 'params': {'pattern': pattern}},
+        {'name': 'shuffle', 'params': {}},
+        {'name': 'limit', "params": {"nb": 10}},
+        {'name': 'imageread', 'params': {}},
+        {'name': 'normalize_shape', 'params': {}},
+        {'name': 'crop', 'params': {'shape': (300, 300), 'pos': 'center', 'mode': 'constant'}},
+        {'name': 'resize', "params": {'shape': (64, 64)}},
+        {'name': 'divide_by', "params": {"value": 255}},
+        #{'name': 'invert', "params": {}},
+        {'name': 'order', "params": {'order': 'th'}}
     ]
-    #iterator = loader({'name': 'cifar', 'params':{'which': 'train'}, 'pipeline':pipeline})
-    pattern = "sketchy/256x256/sketch/tx_000000000000/**/*.png"
-    iterator = loader(
-        { 'name': 'imagecollection',
-           'params': {'pattern': pattern},
-           'pipeline': pipeline
-        }
-    )
+    iterator = loader({'pipeline': pipeline})
     iterator = minibatch(iterator, batch_size=9)
     iterator = expand_dict(iterator)
     iterator = imap(partial(dict_apply, fn=np.array, cols=['X']), iterator)
     X = (next(iterator)['X'])
-    print(X.shape)
     img = disp_grid(X, border=1, bordercolor=(0.3,0,0))
     imsave('out.png', img)
--- jobs/insert_training_jobs.py
+++ jobs/insert_training_jobs.py
@@ -2726,6 +2726,46 @@ def jobset59():
 
     return jobset_recurrent_brush_stroke('jobset59', 'model88', update=update)
 
+def jobset60():
+    # like jobset59 but with crops of sketchy
+    def update(params):
+        rng = random
+        sigma = 1
+        stride = 1
+        model_params = dict(
+            nonlin_out=rng.choice(('linear', 'sigmoid')),
+            reduce_func=rng.choice(('sum', 'over', 'max')),
+            normalize_func='sigmoid',
+            x_sigma=sigma,
+            y_sigma=sigma,
+            x_stride=stride,
+            y_stride=stride,
+            patch_index=0,
+            patch_size=rng.choice((1,2)),
+            color=[1.],
+            x_min=0,
+            x_max='width',
+            y_min=0,
+            y_max='height',
+            recurrent_model='lstm',
+            eps=0,
+            n_steps=rng.randint(1, 100),
+            parallel=rng.choice((1, 2, 3, 4, 5, 6, 7 , 8)),
+            parallel_share=False,
+            parallel_reduce_func='sum',
+        )
+        params['model_params'].update(model_params)
+        params['data_params'] = {
+            'nb_examples': 1000,
+            'transformation': 'crop'
+        }
+        params['dataset'] = 'sketchy'
+        params['force_w'] = 32
+        params['force_h'] = 32
+        return params
+
+    return jobset_recurrent_brush_stroke('jobset60', 'model88', update=update)
+
 @click.command()
 @click.option('--where', default='', help='jobset name', required=False)
 @click.option('--nb', default=1, help='nb of repetitions', required=False)
